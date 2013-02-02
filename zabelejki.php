7733f2331629d1253b836d2a362be205c58460a3
azuneca <ivaniliev93@gmail.com>
2013-02-02 16:37:06 +0100

    Everything up to date with the new release

--- zabelejki.php
+++ zabelejki.php
@@ -20,7 +20,7 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<!-- Copyright 2013 Електронен Дневник All rights reserved. Designed and developed by Azuneca -->
+<!-- Copyright 2013 School diary All rights reserved. Designed and developed by Azuneca -->
 <head>
 	<?php
 		include('scripts/head_tags.php');
